decde8cf46738b27b835a0864a27aff34706fd62
solveig.9@list.ru <solveig.9@list.ru>
2024-12-22 08:06:18 +0300

    eight

--- src/js/index.js
+++ src/js/index.js
@@ -6,12 +6,42 @@ import './sidebar.js'
 import './sidebarSecond.js'
 import './sidebarThird.js'
 
-/* core version + navigation, pagination modules:
-import Swiper from 'swiper'
-import { Navigation, Pagination } from 'swiper/modules'
-// import Swiper and modules styles
-import 'swiper/css'
-import 'swiper/css/navigation'
-import 'swiper/css/pagination' */
+/*
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle'
 
-console.log('Works!')
+// import styles bundle
+import 'swiper/css/bundle'
+
+// init Swiper:
+const swiper = new Swiper(...)
+*/
+
+
+/*
+// import Swiper JS
+import Swiper from 'swiper';
+// import Swiper styles
+import 'swiper/css';
+
+const swiper = new Swiper(...); 
+*/
+
+/*
+// core version + navigation, pagination modules:
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+  // configure Swiper to use modules
+  modules: [Navigation, Pagination],
+  ...
+}); 
+*/
+
+//console.log('Works!')
--- src/js/swiperBrands.js
+++ src/js/swiperBrands.js
@@ -1,6 +1,6 @@
 export const myVariable = 'CPSfinal'
 
-const swiper = new Swiper('.swiper-container', {
+const swiper = new Swiper('.swiper', {
   loop: true,
   slidesPerView: 'auto',
   spaceBetween: 20,
@@ -10,7 +10,7 @@ const swiper = new Swiper('.swiper-container', {
   }
 })
 
-const swiperContainer = document.querySelector('.swiper-container')
+const swiperContainer = document.querySelector('.swiper')
 const swiperWrapper = document.querySelector('.swiper-wrapper')
 const swiperSlide = document.querySelectorAll('.swiper-slide')
 
@@ -18,13 +18,13 @@ window.addEventListener('resize', function () {
   const windowWidth = window.innerWidth
 
   if (windowWidth >= 768) {
-    swiperContainer.classList.remove('swiper-container')
+    swiperContainer.classList.remove('swiper')
     swiperWrapper.classList.remove('swiper-wrapper')
     swiperSlide.forEach(function (slide) {
       slide.classList.remove('swiper-slide')
     })
   } else {
-    swiperContainer.classList.add('swiper-container')
+    swiperContainer.classList.add('swiper')
     swiperWrapper.classList.add('swiper-wrapper')
     swiperSlide.forEach(function (slide) {
       slide.classList.add('swiper-slide')
