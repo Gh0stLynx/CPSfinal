dbd31e881a341fea17ffc68b0a89ff72caee97d2
solveig.9@list.ru <solveig.9@list.ru>
2024-12-25 05:20:33 +0300

    theerteen

--- src/js/index.js
+++ src/js/index.js
@@ -12,6 +12,16 @@ import './sidebarThird.js'
 // import styles bundle
 //import 'swiper/css/bundle'
 
+/*const swiper = new Swiper('.swiper', {
+  loop: true,
+  slidesPerView: 'auto',
+  spaceBetween: 20,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  }
+})*/
+
 // init Swiper:
 
 /*const swiper = new Swiper('.swiper', {
@@ -26,34 +36,29 @@ import './sidebarThird.js'
 
 //const swiper = new Swiper(...)
 
-
 /*import Swiper, { Navigation, Pagination } from 'swiper';
 import 'swiper/swiper-bundle.css'; // Подключаем стили Swiper */
 
-/*
 // import Swiper JS
-import Swiper from 'swiper';
+//import Swiper from 'swiper'
 // import Swiper styles
-import 'swiper/css';
+//import 'swiper/css'
 
-const swiper = new Swiper(...); 
-*/
+//const swiper = new Swiper(...);
 
-/*
 // core version + navigation, pagination modules:
-import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
+/*import Swiper from 'swiper'
+import { Navigation, Pagination } from 'swiper/modules'
 // import Swiper and modules styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'*/
 
 // init Swiper:
-const swiper = new Swiper('.swiper', {
+/*const swiper = new Swiper('.swiper', {
   // configure Swiper to use modules
   modules: [Navigation, Pagination],
   ...
-}); 
-*/
+}); */
 
 //console.log('Works!')
